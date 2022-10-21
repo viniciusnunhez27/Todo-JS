379d87cc81bc8d90babbe94b59597209908dbbb5
viniciusnunhez27 <v.nunhez27@gmail.com>
2022-10-21 11:19:20 -0300

    Remove TODO

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ formAddTodo.addEventListener('submit', event => {
     event.preventDefault()
 
 const inputValue = event.target.add.value.trim()
- if(inputValue.length){
-    todosContainer.innerHTML += ` 
+  if(inputValue.length) {
+     todosContainer.innerHTML += ` 
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <span>${inputValue}</span>
      <i class="far fa-trash-alt delete"></i>
       </li>
      `
- }
+  
+     event.target.reset()
+  }
+})
 
-
-
+todosContainer.addEventListener('click', event => {
+   const clickedElement = event.target
+  if(Array.from(clickedElement.classList).includes('delete')) {
+    clickedElement.parentElement.remove()
+  }
 })
