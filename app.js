fc38e5385deea79066fd8fbce58907d827e9e266
viniciusnunhez27 <v.nunhez27@gmail.com>
2022-10-24 19:56:08 -0300

    Refatoring Todo

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ formAddTodo.addEventListener('submit', event => {
 const inputValue = event.target.add.value.trim()
   if(inputValue.length) {
      todosContainer.innerHTML += ` 
-    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
       <span>${inputValue}</span>
-     <i class="far fa-trash-alt delete"></i>
+     <i class="far fa-trash-alt delete" data-trash="${inputValue}"></i>
       </li>
      `
   
@@ -21,9 +21,9 @@ const inputValue = event.target.add.value.trim()
 
 todosContainer.addEventListener('click', event => {
    const clickedElement = event.target
-  if(Array.from(clickedElement.classList).includes('delete')) {
-    clickedElement.parentElement.remove()
-  }
+   if(clickedElement.dataset.trash) {
+(document.querySelector(`[data-todo="${clickedElement.dataset.trash}"]`)).remove()
+   }
 })
 
 
